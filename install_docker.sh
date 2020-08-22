d8f8fefaf0a467bcd2159507d5eb0a593e59d8a7
Hazhir Derakhshi <hajear@gmail.com>
2020-08-23 02:07:11 +0430

    Add docker-compose install script

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,9 @@
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io -y
+usermod -aG docker hazhir
